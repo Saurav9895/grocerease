38e21c146012cf673c7aeb819e617b27cebdd847
Saurav Kumar Yadav <iamsauravkumaryadav@gmail.com>
2025-07-01 11:13:53 +0000

    edit it such that every time host name need not to be configured for dif

--- next.config.ts
+++ next.config.ts
@@ -12,15 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
